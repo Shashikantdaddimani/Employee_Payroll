d7e83a890433666948035f06d106a0529411198d
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2024-02-01 13:20:13 +0530

    Claculating the employee wage

--- src/com/java/employee/Present_OR_Absent.java
+++ src/com/java/employee/Present_OR_Absent.java
@@ -3,13 +3,21 @@ package com.java.employee;
 public class Present_OR_Absent {
 
 	public static void main(String[] args) {
-		int IS_FULL_TIME =1;
+		int IS_FULL_TIME = 1;
+		int Emp_Rate_per_Hour=20;
+		//Veriables
+		int empHRS = 0;
+		int empWages=0;
+		
 		double empCheck=Math.floor(Math.random()*10)%2;
-		if(empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is present");
+		if(empCheck==IS_FULL_TIME) {
+			empHRS=8;
 		}else {
-			System.out.println("Employee is absent");
+			empHRS=0;
+			
 		}
-	}
+		empWages=empHRS*Emp_Rate_per_Hour;
+		System.out.println("Emp Wage :"+empWages);
 
+	}
 }
